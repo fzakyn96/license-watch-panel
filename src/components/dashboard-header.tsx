167a2cc8e7398d16c4625d4107f6aed09025641a
Farid Zaky N <fzakyn96@gmail.com>
2025-08-13 11:11:33 +0700

    update env, vite, dashboard-header

--- src/components/dashboard-header.tsx
+++ src/components/dashboard-header.tsx
@@ -128,7 +128,7 @@ export const DashboardHeader = ({ onLogout }: DashboardHeaderProps) => {
           {/* Logo */}
           <div className="flex items-center space-x-2 sm:space-x-3">
             <img 
-              src={theme === 'dark' ? '/logo-dark.png' : '/logo-white.png'} 
+              src={theme === 'dark' ? `${import.meta.env.VITE_BASE_NAME}/logo-dark.png` : `${import.meta.env.VITE_BASE_NAME}/logo-white.png`} 
               alt="Lisensi Aset Logo"
               className="h-8 w-auto sm:h-8 object-contain"
             />
