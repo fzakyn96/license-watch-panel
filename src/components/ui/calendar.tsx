ddb0c1335475da21d6406f283418675b19233cd6
Farid Zaky N <fzakyn96@gmail.com>
2025-08-14 12:31:13 +0700

    fix selected date

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -43,7 +43,7 @@ function Calendar({
         ),
         day_range_end: "day-range-end",
         day_selected:
-          "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground [&:not([aria-selected])]:text-foreground",
+          "bg-success text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-success focus:text-primary-foreground [&:not([aria-selected])]:text-foreground",
         day_today: "bg-accent text-accent-foreground font-semibold",
         day_outside:
           "day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30",
