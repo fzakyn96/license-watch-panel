137ffcf8832eaac54a7d4434c6e55db1a56e8707
Farid Zaky N <fzakyn96@gmail.com>
2025-09-02 23:09:51 +0700

    fix env and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.ISDEV === 'true' ? "/lisa/" : "/lisa",
+  base: process.env.VITE_ISDEV === 'true' ? "/lisa/" : "/lisa",
   server: {
     host: "::",
     port: 8080,
